8e73bbb2f9884a567f07fa201a9e235f89e528c2
redcarpy <redcarpy@hotmail.com>
2025-03-21 00:18:00 -0600

    edit: testcomponent vue is now consuming api form the backend

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,21 +1,32 @@
 <template>
   <h1>TestPage</h1>
   <p>{{name}}</p>
+  <p>{{message }}</p>
+  <form v-on:submit="updateMessage">
+    <input type="text" >
+    <input type="submit">
+  </form>
 </template>
 <script>
+import axios from 'axios'
 export default {
+    
     data() {
     return {
        name: "Nope",
+       message: ""
     };
   },
   methods: {
     updateMessage() {
       this.message = "You clicked the button!";
+      
     }
   },
   mounted(){
         this.name = "Richard"
+        axios.get("http://localhost:8080/products")
+        .then(res=> console.log(res.data))
   },
     name: 'HelloWorld',
     props: {
