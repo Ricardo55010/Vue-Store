5dde536f3784b5ab1b69c8601fa291c7f7419e54
redcarpy <redcarpy@hotmail.com>
2025-03-20 23:43:34 -0600

    Edit: new test component

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>TestPage</h1>
+  <p>{{name}}</p>
+</template>
+<script>
+export default {
+    data() {
+    return {
+       name: "Nope",
+    };
+  },
+  methods: {
+    updateMessage() {
+      this.message = "You clicked the button!";
+    }
+  },
+  mounted(){
+        this.name = "Richard"
+  },
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
+    }
+</script>
+  
+<style>
+</style>
