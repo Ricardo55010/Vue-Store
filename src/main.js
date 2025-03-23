3319b29709c26fbf4c866106da2e652f05ca8ee2
Ricardo55010 <richi_mari.2011@hotmail.com>
2025-03-23 16:28:44 -0600

    edit: group vuetify imports into separe js file

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/Router";
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-  })
-
+import vuetify from './Vuetify';
 createApp(App).use(router).use(vuetify).mount('#app')
